adc391ac9c9307778f69135ffeccb640464a5a63
Mihretu145 <mercykoo145@gmail.com>
2023-02-23 13:34:39 -0800

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * print_most_numbers - prints 0 - 9 except 2, 3
@@ -8,15 +8,15 @@
 
 void print_most_numbers(void)
 {
-	int a = 0;
+int a = 0;
 
-	while (a <= 9)
-	{
-		if (a != 2 && a != 4)
-		{
-			_putchar(a + '0');
-		}
-		a++;
-	}
-	_putchar('\n');
+while (a <= 9)
+{
+if (a != 2 && a != 4)
+{
+_putchar(a + '0');
+}
+a++;
+}
+_putchar('\n');
 }
