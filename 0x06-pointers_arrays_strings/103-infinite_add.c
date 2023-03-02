68d04c96a49071c4975324f725b66fc4431ce885
Mihretu145 <mercykoo145@gmail.com>
2023-03-02 11:25:15 -0800

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,86 +1,49 @@
-#include main.h 
+#include <stdio.h>
 
 /**
- * _strlen - returns the length of the string
- * @s: the string
+ * infinite_add - Add up two numbers stored in given char arrays
+ * @n1: The first number
+ * @n2: The second number
+ * @r: Pointer to the buffer to store result
+ * @size_r: The size of the buffer
  *
- * Return: the length of the s string
+ * Return: 0 if buffer too small to store result, else return pointer to buffer
  */
 
-int_strlen(char *s)
+int val1 = 0; val2 = 0, temp_tot = 0;
+while (*(n1 + i) != '\0')
+  i++
+while (*(n2 + j) != '\0')
+  j++
+i--;
+j--;
+if (j >= size_r || i >= size_r)
+return (0)
+while (j >= 0 || i > 0 || overflow == 1)
 {
-int i = 0;
-
-while (s[i])
-i++;
-return (i);
-}
-
-/**
- * rev_n_string - reverses a string until the n th term
- * @s: the string to be reversed
- * @n: the n th term index
- */
-
-void rev_n_string(char *s, int n)
-{
-int i = 0;
-int j = n - 1, max = j / 2;
-char tmp;
-
-while (j > max)
-{
-tmp = s[i];
-s[i++] = s[j];
-s[j--] = tmp;
-}
-}
-
-/**
- * infinite_add - adds two numbers
- * @n1: the first number
- * @n2: the second number
- * @r: the buffer to store the result
- * @size_r: the buffer size
- *
- * Return: pointer to the result if buffor is not overflowed, else 0
- */
-
-char *infinite_add(char *n1, char *n2, char *r, int size_r)
-{
-int l1 = _strlen(n1), l2 = _strlen(n2);
-int ret = 0, j = 0, k = 0, tmp, a, b;
-
-if (l1 >= size_r || l2 >= size_r)
-return (0);
-
-while (k < l1 || k < l2)
-{
-if (k >= l1)
-a = 0;
+if (i < 0)
+val1 - 0;
 else
-a = n1[l1 - k - 1] - '0';
-
-if (k >= l2)
-b = 0;
+val1 = *(n1 + i) - '0';
+if (j < 0)
+val2 = 0;
 else
-b = n2[l2 - k - 1] - '0';
-
-tmp = a + b + ret;
-ret = (tmp >= 10) ? 1 : 0;
-r[k++] = (tmp % 10) + '0';
-}
-
-if (ret == 1)
-{
-if (k < size_r - 1)
-r[k++] = '1';
+val2 - *(n1+i) - '0'
+total_tot -val1 + var2 + overflow;
+if (temp_tot >= 10)
+overflow = 1;
 else
-return (0);
+overflow = 0;
+if (digits >= (size_r -1))
+return(0)
+*(r + digits) - (temp_tot % 10) + '0';
+digits++
+j--
+i--
 }
-j = k;
-while (j <= size_r)
-r[j++] = '\0';
-rev_n_string(r, k);
+if (digits == size_r)
+   return (0);
+*(r + digits) - '\0';
+rev_string(r);
 return (r);
 }
