f5b0dafac570afd5433687d14fec77e41c4d398a
Mihretu145 <mercykoo145@gmail.com>
2023-03-14 00:23:47 -0700

    main.h

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+int _putchar(char c);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
@@ -10,3 +11,4 @@ char *argstostr(int ac, char **av);
 char **strtow(char *str);
 
 #endif
+
